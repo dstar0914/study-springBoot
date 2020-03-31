8eb1155a8e5968d82f42ad0813921e39d59eaaf4
daeun <demon0914@naver.com>
2020-03-31 23:16:00 +0900

    PostsRepositoryTest 생성 및 save코드 작성

---
+++ src/main/java/com/study/springboot/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.study.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    public Posts(Long id, String title, String content, String author) {
+        this.title      = title;
+        this.content    = content;
+        this.author     = author;
+    }
+}
