237bc86186c68c58bf841cac71d6a07686b82138
daeun <demon0914@naver.com>
2020-04-10 01:53:39 +0900

    게시판 CRUD 구현 [오류] Request method 'DELETE' not supported 아직 해결 못함

--- src/main/java/com/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.study.springboot.service.posts;
 
 import com.study.springboot.domain.posts.Posts;
 import com.study.springboot.domain.posts.PostsRepository;
+import com.study.springboot.web.dto.PostsListResponseDto;
 import com.study.springboot.web.dto.PostsResponseDto;
 import com.study.springboot.web.dto.PostsSaveRequestDto;
 import com.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -41,5 +44,20 @@ public class PostsService {
                 );
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
